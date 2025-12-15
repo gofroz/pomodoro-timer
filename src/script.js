15c8aa27612310817c77b1e233971e5e77273f5a
“gofroz” <gofroz@gmail.com>
2025-12-15 16:39:55 -0400

    first commit

---
+++ src/script.js
@@ -0,0 +1,42 @@
+    // etapas relogio pomodoro
+    // tempo de foco: 25 minutos
+    // pausa curta: 5 minutos
+    // pausa longa: 15 minutos
+    // ciclos com pausa longa: geralmente 4 ciclos
+    // a pessoa pode gerenciar quantos ciclos deseja? sim ou não *verificar
+
+    // tempoRestante = em segundos
+    // modoAtual = foco pausaCurta pausaLonga
+    // ciclosCompletos = numero inteiro
+    // timerAtivo = true or false
+
+    const tempoDisplay = document.getElementById("tempo-em-segundos");
+    const iniciar = document.getElementById("iniciar");
+    let tempoRestante = 25 * 60;
+    let timer = null;
+
+    function atualizaDisplay(){
+        const minutos = Math.floor(tempoRestante / 60);
+        const segundos = tempoRestante % 60;
+        
+        const segundosFormatados = segundos < 10 ? "0" + segundos : segundos;
+
+        tempoDisplay.textContent = minutos + ":" + segundosFormatados;
+    
+    }
+    function inicio(){
+        if (timer !== null) return;
+
+        timer = setInterval(() => {
+            tempoRestante--;
+
+            atualizaDisplay();
+
+            if(tempoRestante <= 0){
+                clearInterval(timer);
+                timer = null;
+                console.log("Ciclo Finalizado");
+            }
+        }, 1000);
+    }
+   iniciar.addEventListener("click", inicio);
